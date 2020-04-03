27d83fba9ae111983fc574d665b665073bfc2a2f
mk <mk@morula.minitrue.eu>
2020-04-03 16:40:30 +0300

    1.6 unicafe step1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = (props) =>  (
+  <div>
+    <div>{props.name} {props.value}</div>
+  </div>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const handleGoodClick = () => {  
+    setGood(good + 1)  
+  }
+  const handleNeutralClick = () => {  
+    setNeutral(neutral + 1)  
+  }
+  const handleBadClick = () => {  
+    setBad(bad + 1)  
+  }
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button text="good" handleClick={handleGoodClick} />
+      <Button text="neutral" handleClick={handleNeutralClick}/>
+      <Button text="bad" handleClick={handleBadClick}/>
+      <h2>Statistic</h2>
+      <Display value={good} name="good"/>
+      <Display value={neutral} name="neutral"/>
+      <Display value={bad} name="bad"/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
