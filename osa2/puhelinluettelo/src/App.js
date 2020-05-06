b633ed422303ea5fe7aa7ddad23ee4c1042a927a
mk <mk@morula.minitrue.eu>
2020-05-06 18:12:52 +0300

    puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
@@ -15,15 +16,20 @@ const App = () => {
   const handleNumChange = (event) => {
     setNewNumber(event.target.value)
   }
-
-  const [persons, setPersons]       = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons]       = useState([])
   const [ newName, setNewName ]     = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  // [] Render also 1st time
 
   const addNum = (event) => {
     event.preventDefault()
