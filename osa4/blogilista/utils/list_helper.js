8299b8747a29cfb01371305a10aa7516383b8ce4
mk <mk@morula.minitrue.eu>
2020-05-15 13:15:32 +0300

    apufunktioita ja yksikkötestejä, step2

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,6 +1,15 @@
 
 const dummy = () => { return 1 }
 
+const totalLikes = (blogs) => {
+  // console.log(blogs)
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
