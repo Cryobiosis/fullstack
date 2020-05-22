8bd9c62d7f0683dd7bfc5f858f087bdef20b9254
mk <mk@morula.minitrue.eu>
2020-05-22 12:08:28 +0300

    blogilistan frontend, step2

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -21,6 +21,11 @@ const App = () => {
         username, password,
       })
 
+      // Save token to browsers local storage
+      window.localStorage.setItem(
+        'loggedBlogUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -31,12 +36,33 @@ const App = () => {
       }, 5000)
     }
   }
+  // Get all blog posts on start up
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
-  
+
+  const handleLogout = async (event) => {
+    window.localStorage.setItem('loggedBlogUser', null)
+    //blogService.setToken(null)
+    // setUser(null)
+    // Reload window
+    window.location.reload(false);
+  }
+
+  // Check login information from local storage on start up
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      if (user) {
+        setUser(user)
+        blogService.setToken(user.token)
+      }
+    }  
+  }, [])
+
   const loginForm = () => (     
     <div clas="loginform">
       <h2> Login </h2>
@@ -55,7 +81,8 @@ const App = () => {
   const blogsForm = () => (
     <div class="blogs">
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in</p><button onClick={() => handleLogout()} type="submit">logout</button>
+
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
