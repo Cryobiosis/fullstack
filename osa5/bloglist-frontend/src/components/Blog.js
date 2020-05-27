e976cf5746aa59e6e0f1e8ab7c9ba1bebd7957a2
mk <mk@morula.minitrue.eu>
2020-05-27 14:19:34 +0300

    Blogilistan frontend, step 7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Togglable from '../components/Togglable'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+const Blog = ({ blog }) =>  {
+
+  const [full, setShowFull] = useState(false)
+
+  const blogRef = React.createRef()
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  return (
+  <div style={blogStyle}>
+    {blog.title}
+
+    {full === false ?
+      <div>
+        <button onClick={() => setShowFull(true)} type="submit">show</button>
+      </div> : 
+      <div>
+        <p>{blog.author}</p>
+        <p>{blog.url}</p>
+        <div>likes TODO: <button onClick={() => console.log('TODO: like')} type="submit">like</button></div>
+        <button onClick={() => setShowFull(false)} type="submit">hide</button>
+
+      </div>
+    }
+
   </div>
-)
+)}
 
 export default Blog
