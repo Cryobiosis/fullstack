069d957ff8bffeff1f0f662d4cb4c38bf2db12a9
mk <mk@morula.minitrue.eu>
2020-05-28 13:01:43 +0300

    Reset api for 5.17

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -6,7 +6,6 @@ const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -31,6 +30,12 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+// ONLY in test env..
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/test')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
