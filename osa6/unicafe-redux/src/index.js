4464da7f0c60dae806fb4e69350eeb95d33c50bb
mk <mk@morula.minitrue.eu>
2020-05-29 20:59:09 +0300

    6.1: unicafe revisited, step2

--- osa6/unicafe-redux/src/index.js
+++ osa6/unicafe-redux/src/index.js
@@ -5,6 +5,37 @@ import reducer from './reducer'
 
 const store = createStore(reducer)
 
+const StatisticLine = (props) =>  (
+  <tr>
+    <td>{props.name}</td><td>{props.value}{props.suffix}</td>
+  </tr>
+)
+
+const Statistic = ({good, bad, neutral}) =>  {
+
+  let all = bad + good + neutral
+  // Good 1, neutral 0, bad -1
+  // TODO: Optimize
+  let avg = ((1 * good) + (-1 * bad)) / all
+
+  if (good > 0 || bad > 0 || neutral > 0)
+    return (
+      <div>
+        <table>
+          <tbody>
+        <StatisticLine value={good} name="good"/>
+        <StatisticLine value={neutral} name="neutral"/>
+        <StatisticLine value={bad} name="bad"/>
+        <StatisticLine value={all} name="all"/>
+        <StatisticLine value={avg} name="average"/>
+        <StatisticLine value={good / all * 100} name="positive" suffix=" %"/>
+        </tbody>
+      </table>
+      </div>)
+    else 
+      return <div>No feedback given</div>
+}
+
 const App = () => {
   const good = () => {
     store.dispatch({
@@ -28,6 +59,8 @@ const App = () => {
   }
   return (
     <div>
+       <h2>give feedback</h2>
+          
       <button onClick={good}>good</button> 
       <button onClick={ok}>neutral</button> 
       <button onClick={bad}>bad</button>
@@ -35,6 +68,10 @@ const App = () => {
       <div>good {store.getState().good}</div>
       <div>neutral {store.getState().ok}</div>
       <div>bad {store.getState().bad}</div>
+
+      <h2>Statistic</h2>
+      <Statistic bad={store.getState().bad} good={store.getState().good} neutral={store.getState().ok}/>
+
     </div>
   )
 }
