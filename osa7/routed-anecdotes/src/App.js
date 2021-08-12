3ce63897a7434bb276a6807cbc54b8f30c31cb57
mk <mk@morula.minitrue.eu>
2021-08-12 17:08:26 +0300

    7.4: anekdoottisovellus ja hookit step1

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Switch, Route, Link, useRouteMatch, useHistory
 } from 'react-router-dom'
+import  { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -74,18 +75,23 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor]   = useState('')
-  const [info, setInfo]       = useState('')
+  //const [content, setContent] = useState('')
+  //const [author, setAuthor]   = useState('')
+  //const [info, setInfo]       = useState('')
   // const [notification, setNotification] = useState('aaa')
   // const history               = useHistory()
+  const content = useField('text')
+  const author  = useField('text')
+  const info    = useField('text')
+
+  console.log(content.value)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     // setNotification(`a new anecdote ${content} created!`)
@@ -93,23 +99,21 @@ const CreateNew = (props) => {
     // Redirect to front page
     // history.push('/')
   }
-
+ 
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          content: <input {...content} /> 
         </div>
         <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          author: <input {...author} /> 
         </div>
         <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          url for more info: <input {...info} /> 
         </div>
+
         <button>create</button>
       </form>
     </div>
