d9f36a00cfee0d90e5521da561dcdb9a79ab2494
mk <mk@morula.minitrue.eu>
2021-08-13 12:21:57 +0300

    7.8*: ultimate hooks

--- osa7/ultimate-hooks/src/App.js
+++ osa7/ultimate-hooks/src/App.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
 const useField = (type) => {
   const [value, setValue] = useState('')
 
@@ -18,12 +17,32 @@ const useField = (type) => {
 }
 
 const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+  }
+
+  // Init items
+  useEffect(() => {
+    if (!baseUrl) return false;
+    getAll().then(items =>
+      setResources( items )
+    )
+  }, []) 
+
+  // Can't init items here, web get infiniteloop, or axios will return promise, use useEffect
+  const [resources, setResources] = useState([])
+  // console.log(resources)
 
   const create = (resource) => {
-    // ...
+    const token = ''
+    const config = {
+      headers: { Authorization: token },
+    }
+  
+    const response = axios.post(baseUrl, resource, config)
+    return response.data
   }
 
   const service = {
@@ -31,7 +50,7 @@ const useResource = (baseUrl) => {
   }
 
   return [
-    resources, service
+    resources, service, getAll
   ]
 }
 
@@ -42,10 +61,12 @@ const App = () => {
 
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
-
+  
+  // console.log('notes',notes)
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
+    // TODO: This should refresh notes from remote server, so we can see new one...
   }
  
   const handlePersonSubmit = (event) => {
