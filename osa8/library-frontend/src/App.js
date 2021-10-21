d9bcda03a9b05d1eac701b8298f943297c335d22
mk <mk@morula.local>
2021-10-21 13:25:32 +0300

    8.8: Kirjailijoiden näkymä

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -2,10 +2,25 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { gql, useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from './queries'
+
+/*
+client.query({ ALL_AUTHORS })
+  .then((response) => {
+    console.log(response.data)
+})*/
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const result = useQuery(ALL_AUTHORS)
 
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  console.log(result)
+  
   return (
     <div>
       <div>
@@ -15,6 +30,7 @@ const App = () => {
       </div>
 
       <Authors
+        authors = {result.data.allAuthors}
         show={page === 'authors'}
       />
 
